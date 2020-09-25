f4f054c3f4ac8ab53136856ff0d1e37fb81795b1
pedrochiossi <pchiossi@gmail.com>
2020-09-25 09:59:04 -0300

    feature - add carousel with custom controls

---
+++ src/index.js
@@ -0,0 +1,14 @@
+window.onload = () => {
+  
+  const slider = tns({
+    container: '.my-slider',
+    items: 6,
+    autoplay: false,
+    loop: false,
+    nav: false,
+    slideBy: 'page',
+    mouseDrag: true,
+    controlsContainer: '#customize-controls'
+  });
+
+}

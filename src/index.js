b99251a297c35496461fd26e598596ed7acf94ba
pedrochiossi <pchiossi@gmail.com>
2020-09-30 12:30:36 -0300

    feature - add favorite icon, add click effect

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
-window.onload = () => {
-  
-  const slider = tns({
-    container: '.my-slider',
+window.onload = () => {  
+  const popularSlider = tns({
+    container: '.popular-slider',
     items: 6,
     autoplay: false,
     loop: false,
     nav: false,
     slideBy: 'page',
     mouseDrag: true,
-    controlsContainer: '#customize-controls'
+    controlsContainer: '#customize-controls-popular'
   });
 
+  const exibitionSlider = tns({
+    container: '.exibition-slider',
+    items: 6,
+    autoplay: false,
+    loop: false,
+    nav: false,
+    slideBy: 'page',
+    mouseDrag: true,
+    controlsContainer: '#customize-controls-exibition'
+  });
+
+  const hearts = document.querySelectorAll('.favorite');
+
+  hearts.forEach((heart) => {
+    heart.onclick = (e) => {
+      e.target.classList.toggle('liked');
+    }
+  })
 }
